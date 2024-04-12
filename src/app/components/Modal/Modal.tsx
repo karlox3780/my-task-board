a3d9b9bbcd63b93dad2db2476f71bd7f404c1ac6
Carlos Bustos Ramiro <carlosbustosramiro@gmail.com>
2024-04-12 23:56:05 +0200

    Obteniendo datos del formulario.

--- src/app/components/Modal/Modal.tsx
+++ src/app/components/Modal/Modal.tsx
@@ -22,6 +22,26 @@ const Modal = ({ onClose, title }: Props) => {
     };
     const [activeIcon, setActiveIcon] = useState();
 
+    const [formState, setFormState] = useState({
+        taskName: "",
+        description: ""
+    });
+
+    function handleChange(e: any) {
+        setFormState({ ...formState, [e.target.name]: e.target.value });
+        console.log(formState);
+    }
+
+    async function handleSubmit(e: any) {
+        e.preventDefault();
+
+        let formData = new FormData();
+
+        for (let [key, value] of Object.entries(formState)) {
+            formData.append(key, value);
+        }
+        console.log(formData);
+    }
     const modalContent = (
         <div className="modal-overlay">
             {/* Wrap the whole Modal inside the newly created StyledModalWrapper
@@ -35,61 +55,65 @@ const Modal = ({ onClose, title }: Props) => {
                         </a>
                     </div>
                     <div className="modal-body">
-                        <div className="mt-[16px]">
-                            <label className="font-medium text-[12px] text-[#97A3B6]">Task name</label>
-                            <input className="w-full border-[1px] border-[#cdd5e0] rounded-[12px] mt-[6px] p-[10px] font-regular text-[16px] focus:outline-[#3662E3]" type="text" placeholder="Enter a task name" />
-                        </div>
-                        <div className="mt-[20px]">
-                            <label className="font-medium text-[12px] text-[#97A3B6]">Description</label>
-                            <textarea className="w-full h-[200px] border-[1px] border-[#cdd5e0] rounded-[12px] mt-[6px] p-[10px] font-regular text-[16px] focus:outline-[#3662E3] resize-none" placeholder="Enter a short description"></textarea>
-                        </div>
-                        <div className="mt-[20px]">
-                            <label className="font-medium text-[12px] text-[#97A3B6]">Icon</label>
-                            <div className="flex gap-[12px]">
-                                {
-                                    icons.map((icon, index) => (
-                                        <span key={icon}>
-                                            <input type="radio" id={"icon-button" + index} name="icon-button" value={icon} className="hidden peer" />
-                                            <label htmlFor={"icon-button" + index}
-                                                className="w-[44px] h-[44px] flex justify-center items-center mt-[6px] text-[20px] rounded-[12px] bg-[#e3e8ef] cursor-pointer peer-checked:bg-[#F5D565]">
-                                                {icon}
-                                            </label>
-                                        </span>
-                                    ))
-                                }
+                        <form onSubmit={handleSubmit}>
+                            <div className="mt-[16px]">
+                                <label className="font-medium text-[12px] text-[#97A3B6]">Task name</label>
+                                <input className="w-full border-[1px] border-[#cdd5e0] rounded-[12px] mt-[6px] p-[10px] font-regular text-[16px] focus:outline-[#3662E3]"
+                                    name="taskName" type="text" placeholder="Enter a task name" onChange={handleChange} required />
                             </div>
-                        </div>
-                        <div className="mt-[20px]">
-                            <label className="font-medium text-[12px] text-[#97A3B6]">Status</label>
-                            <div className="grid grid-cols-2 gap-x-[16px] gap-y-[6px] w-full">
-                                {status.map((status, index) => (
-                                    <div className="cursor-pointer" key={index}>
-                                        <input type="radio" id={"status-button" + index} name="status-button" value={status.label} className="hidden peer/status" />
-                                        <label htmlFor={"status-button" + index} className="w-full flex p-[2px] items-center justify-between cursor-pointer rounded-[12px] border border-[#e3e8ef] peer-checked/status:border-[#3662E3]">
-                                            <div className="flex items-center">
-                                                <div className={"p-[10px] rounded-[12px] mr-[10px] bg-[" + status.color + "]"}>
-                                                    <Image width="20" height="20" src={status.icon} alt="Status Icon" />
+                            <div className="mt-[20px]">
+                                <label className="font-medium text-[12px] text-[#97A3B6]">Description</label>
+                                <textarea className="w-full h-[200px] border-[1px] border-[#cdd5e0] rounded-[12px] mt-[6px] p-[10px] font-regular text-[16px] focus:outline-[#3662E3] resize-none"
+                                    name="description" placeholder="Enter a short description" onChange={handleChange}></textarea>
+                            </div>
+                            <div className="mt-[20px]">
+                                <label className="font-medium text-[12px] text-[#97A3B6]">Icon</label>
+                                <div className="flex gap-[12px]">
+                                    {
+                                        icons.map((icon, index) => (
+                                            <span key={icon}>
+                                                <input type="radio" id={"icon-button" + index} name="icon-button" value={icon} className="hidden peer" />
+                                                <label htmlFor={"icon-button" + index}
+                                                    className="w-[44px] h-[44px] flex justify-center items-center mt-[6px] text-[20px] rounded-[12px] bg-[#e3e8ef] cursor-pointer peer-checked:bg-[#F5D565]">
+                                                    {icon}
+                                                </label>
+                                            </span>
+                                        ))
+                                    }
+                                </div>
+                            </div>
+                            <div className="mt-[20px]">
+                                <label className="font-medium text-[12px] text-[#97A3B6]">Status</label>
+                                <div className="grid grid-cols-2 gap-x-[16px] gap-y-[6px] w-full">
+                                    {status.map((status, index) => (
+                                        <div className="cursor-pointer" key={index}>
+                                            <input type="radio" id={"status-button" + index} name="status-button" value={status.label} className="hidden peer/status" />
+                                            <label htmlFor={"status-button" + index} className="w-full flex p-[2px] items-center justify-between cursor-pointer rounded-[12px] border border-[#e3e8ef] peer-checked/status:border-[#3662E3]">
+                                                <div className="flex items-center">
+                                                    <div className={"p-[10px] rounded-[12px] mr-[10px] bg-[" + status.color + "]"}>
+                                                        <Image width="20" height="20" src={status.icon} alt="Status Icon" />
+                                                    </div>
+                                                    <label className="cursor-pointer" htmlFor={"status-button" + index}>{status.label}</label>
                                                 </div>
-                                                <label className="cursor-pointer" htmlFor={"status-button" + index}>{status.label}</label>
-                                            </div>
-                                            <label htmlFor={"status-button" + index} className="bg-[#3662E3] rounded-[50%] mr-[10px] hidden done-icon cursor-pointer">
-                                                <Image width="20" height="20" src="/images/Done_round.svg" alt="Status Icon" />
+                                                <label htmlFor={"status-button" + index} className="bg-[#3662E3] rounded-[50%] mr-[10px] hidden done-icon cursor-pointer">
+                                                    <Image width="20" height="20" src="/images/Done_round.svg" alt="Status Icon" />
+                                                </label>
                                             </label>
-                                        </label>
-                                    </div>
-                                ))}
+                                        </div>
+                                    ))}
+                                </div>
                             </div>
-                        </div>
-                        <div className="bottom-buttons flex justify-end mt-[100px] text-[#fff]">
-                            <button className="bg-[#97A3B6] flex px-[30px] py-[10px] rounded-[22px] mr-[16px] cursor-pointer">
-                                <label className="mr-[5px] text-[14px] cursor-pointer">Delete</label>
-                                <Image width="20" height="20" src="/images/Trash.svg" alt="Status Icon" />
-                            </button>
-                            <button className="bg-[#3662E3] flex px-[30px] py-[10px] rounded-[22px] cursor-pointer">
-                                <label className="mr-[5px] text-[14px] cursor-pointer">Save</label>
-                                <Image width="20" height="20" src="/images/Done_round.svg" alt="Status Icon" />
-                            </button>
-                        </div>
+                            <div className="bottom-buttons flex justify-end mt-[100px] text-[#fff]">
+                                <button className="bg-[#97A3B6] flex px-[30px] py-[10px] rounded-[22px] mr-[16px] cursor-pointer">
+                                    <label className="mr-[5px] text-[14px] cursor-pointer">Delete</label>
+                                    <Image width="20" height="20" src="/images/Trash.svg" alt="Status Icon" />
+                                </button>
+                                <button className="bg-[#3662E3] flex px-[30px] py-[10px] rounded-[22px] cursor-pointer">
+                                    <label className="mr-[5px] text-[14px] cursor-pointer">Save</label>
+                                    <Image width="20" height="20" src="/images/Done_round.svg" alt="Status Icon" />
+                                </button>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
